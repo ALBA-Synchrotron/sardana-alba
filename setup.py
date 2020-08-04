63944e7783d0a70736273067bbec2a666f0f2ea4
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-08-04 18:11:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import sys
+from setuptools import setup, find_packages
+
+
+setup(
+    name="sardana-alba",
+    version="0.1.0",
+    description="ALBA specific sardana tools (controllers, macros, tools)",
+    author="ALBA controls team",
+    author_email="controls@cells.es",
+    license="GPLv3",
+    url="http://github.com/ALBA-Synchrotron/sardana-alba",
+    packages=find_packages(),
+    install_requires=["sardana", "tangoctl>=0.6"],
+    python_requires=">=3.5",
+)
