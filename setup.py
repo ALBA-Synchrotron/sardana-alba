fda570dd48e2f39ab1a274c3891e0810ea4ecef1
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-08-04 18:33:22 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sardana-alba",
-    version="0.1.0",
+    version="0.2.0",
     description="ALBA specific sardana tools (controllers, macros, tools)",
     author="ALBA controls team",
     author_email="controls@cells.es",
