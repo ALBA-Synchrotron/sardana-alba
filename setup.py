65ea0431a12777005ff14d64f952d9a881e0368d
Roberto Javier Homs Puron <rhoms@cells.es>
2022-07-15 08:08:00 +0200

    Bump version: 0.6.3 → 0.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sardana-alba",
-    version="0.6.3",
+    version="0.7.0",
     description="ALBA specific sardana tools (controllers, macros, tools)",
     author="ALBA controls team",
     author_email="controls@cells.es",
